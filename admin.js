8363bd995e113c9100e903c79990aa412ad0b3fb
sagilityph <sagilityassessment@gmail.com>
2026-07-11 19:53:01 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,17 +1,20 @@
 const SITE = "qc";
+
 import { db } from "./firebase.js";
 import {
-  ref,
-  set,
-  get,
-  push
+    ref,
+    set,
+    get,
+    push
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
 const g = document.getElementById("grid");
 
 async function loadSeats() {
 
-    const snapshot = await get(ref(db, "seats"));
+    const snapshot = await get(
+        ref(db, `locations/${SITE}/seats`)
+    );
 
     let seats = {};
 
@@ -28,11 +31,17 @@ async function loadSeats() {
         g.innerHTML += `
         <div class="card">
             <h3>Seat ${i}</h3>
-            <input id="seat${i}"
+
+            <input
+                id="seat${i}"
                 value="${v}"
-                onchange="saveSeat(${i}, this.value)">
-            <button id="b${i}"
-                onclick="callSeat(${i})">
+                onchange="saveSeat(${i}, this.value)"
+            >
+
+            <button
+                id="b${i}"
+                onclick="callSeat(${i})"
+            >
                 CALL
             </button>
         </div>
@@ -43,25 +52,35 @@ async function loadSeats() {
 window.saveSeat = async function(seat, value){
 
     await set(
-        ref(db, `seats/${seat}`),
+        ref(
+            db,
+            `locations/${SITE}/seats/${seat}`
+        ),
         value || 0
     );
 
-}
+};
 
 window.callSeat = async function(seat){
 
-    const seatSnapshot =
-        await get(ref(db, `seats/${seat}`));
+    const seatSnapshot = await get(
+        ref(
+            db,
+            `locations/${SITE}/seats/${seat}`
+        )
+    );
 
     let id = 0;
 
-    if(seatSnapshot.exists()){
+    if (seatSnapshot.exists()) {
         id = seatSnapshot.val();
     }
 
     await push(
-        ref(db, "queue"),
+        ref(
+            db,
+            `locations/${SITE}/queue`
+        ),
         {
             seat: seat,
             id: id,
@@ -69,11 +88,12 @@ window.callSeat = async function(seat){
         }
     );
 
-    const btn =
-        document.getElementById("b" + seat);
+    const btn = document.getElementById(
+        "b" + seat
+    );
 
     btn.className = "called";
     btn.innerHTML = "CALLED ✓";
-}
+};
 
 loadSeats();
